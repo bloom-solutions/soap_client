5211b8c85a05f2947720376f785c05dac420f853
Ramon Tayag <ramon.tayag@gmail.com>
2017-02-16 11:01:54 +0800

    Do not allow Savon to log

--- lib/soap_client/services/build_savon_attrs.rb
+++ lib/soap_client/services/build_savon_attrs.rb
@@ -2,12 +2,9 @@ module SOAPClient
   class BuildSavonAttrs
 
     def self.call(raw_attrs)
-      attrs = raw_attrs.slice(
-        :wsdl,
-        :log,
-        :logger,
-      )
+      attrs = raw_attrs.slice(:wsdl)
       attrs[:proxy] = raw_attrs[:proxy] if raw_attrs[:proxy].present?
+      attrs[:log] = false
       attrs
     end
 
